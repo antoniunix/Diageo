1e9ba216365940ca843685816259f17ac9f72586
jose antonio mendoza lopez <antoniunix@gmail.com>
2019-09-27 11:21:29 -0500

    define completable with timeout to change of activity

--- app/src/main/java/com/chuys/gshp/diageo/splash/presenter/SplashPresenter.kt
+++ app/src/main/java/com/chuys/gshp/diageo/splash/presenter/SplashPresenter.kt
@@ -2,22 +2,37 @@ package com.chuys.gshp.diageo.splash.presenter
 
 import android.util.Log
 import com.chuys.gshp.diageo.splash.contract.SplashContract
-import io.reactivex.Observable
+import io.reactivex.Completable
+import io.reactivex.disposables.Disposable
+import io.reactivex.observers.DisposableCompletableObserver
 import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
 
-class SplashPresenter : SplashContract.SplashPresenterContract {
+class SplashPresenter(var view: SplashContract.SplashViewContract) : SplashContract.SplashPresenterContract {
+    private lateinit var disposable: Disposable
+    private val timeOutSplash = 3L
+
     override fun create() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun startCountTime() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        disposable = Completable.complete()
+            .delay(timeOutSplash, TimeUnit.SECONDS, Schedulers.io())
+            .subscribeWith(object : DisposableCompletableObserver() {
+                override fun onError(error: Throwable) {
+                    error.printStackTrace()
+                }
+
+                override fun onComplete() {
+                    view.endTime()
+                }
+            })
     }
 
     override fun finish() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        disposable.dispose()
     }
 
 }
--- app/src/main/java/com/chuys/gshp/diageo/splash/view/Splash.kt
+++ app/src/main/java/com/chuys/gshp/diageo/splash/view/Splash.kt
@@ -2,27 +2,30 @@ package com.chuys.gshp.diageo.splash.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.chuys.gshp.diageo.R
 import com.chuys.gshp.diageo.splash.contract.SplashContract
 import com.chuys.gshp.diageo.splash.presenter.SplashPresenter
 
-class Splash : AppCompatActivity(),SplashContract.SplashViewContract {
+class Splash : AppCompatActivity(), SplashContract.SplashViewContract {
 
+    private val tag: String? = Splash::class.java.simpleName
     private lateinit var presenter: SplashContract.SplashPresenterContract
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        presenter=SplashPresenter()
+        presenter = SplashPresenter(this)
     }
 
     override fun onStart() {
         super.onStart()
         presenter.create()
+        presenter.startCountTime()
     }
 
     override fun endTime() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Log.d(tag, "change to other module")
     }
 
     override fun onDestroy() {
