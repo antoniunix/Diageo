94abac1b340228ac4bd988400fb4027794fce552
jose antonio mendoza lopez <antoniunix@gmail.com>
2019-10-02 00:51:56 -0500

    implement view pdv

--- pdv/src/main/java/com/chuys/gshp/pdv/data/repository/PdvDataRepository.kt
+++ pdv/src/main/java/com/chuys/gshp/pdv/data/repository/PdvDataRepository.kt
@@ -1,6 +1,5 @@
 package com.chuys.gshp.pdv.data.repository
 
-import android.util.Log
 import com.chuys.gshp.pdv.domain.model.PdvData
 import com.chuys.gshp.pdv.domain.repository.PdvRepository
 import com.chuys.gshp.shared.domain.constant.StringConstant
@@ -13,16 +12,23 @@ class PdvDataRepository : PdvRepository {
     override fun getAllPdv(): Single<Resource<List<PdvData>>> {
         //This method must change by realtime
         val pdvData = PdvData(
-            1, "", 1,
-            "", "", "CODEE", 1,
+            1, "PDV1", 1,
+            "CLIENTE1", "", "CODEE", 1,
             "", "", "",
             "", "", "", 0.0, 0.0
         )
-        return Single.just(listOf(pdvData)).map {
-            result->
-            when{ !result.isEmpty()->{
-                return@map Resource.success(result,StringConstant.EMPTY_STRING)
-            }else-> Resource.error<List<PdvData>>(StringConstant.DONT_HAVE_DATA_IN_DATABASE_ESP)
+        val pdvData2 = PdvData(
+            2, "PDV2", 2,
+            "CLIENTE2", "", "CODEE", 1,
+            "", "", "",
+            "", "", "", 0.0, 0.0
+        )
+        return Single.just(listOf(pdvData,pdvData2)).map { result ->
+            when {
+                !result.isEmpty() -> {
+                    return@map Resource.success(result, StringConstant.EMPTY_STRING)
+                }
+                else -> Resource.error<List<PdvData>>(StringConstant.DONT_HAVE_DATA_IN_DATABASE_ESP)
 
             }
         }
--- pdv/src/main/java/com/chuys/gshp/pdv/view/ListPdv.kt
+++ pdv/src/main/java/com/chuys/gshp/pdv/view/ListPdv.kt
@@ -2,9 +2,12 @@ package com.chuys.gshp.pdv.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.chuys.gshp.pdv.R
 import com.chuys.gshp.pdv.contract.PdvContract
 import com.chuys.gshp.pdv.data.provider.PdvDataProvider
+import com.chuys.gshp.pdv.domain.model.PdvData
 import com.chuys.gshp.pdv.domain.providers.PdvProvider
 import com.chuys.gshp.pdv.presenter.Presenter
 import com.chuys.gshp.shared.data.job.JobExecutor
@@ -14,6 +17,8 @@ class ListPdv : AppCompatActivity(), PdvContract.PdvViewContract {
 
     private lateinit var pdvProvider: PdvProvider
     private lateinit var presenter: PdvContract.PdvPresenterContract
+    private lateinit var listPdvRecyclerView: RecyclerView
+    private var listAdapter: ListPdvAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +27,7 @@ class ListPdv : AppCompatActivity(), PdvContract.PdvViewContract {
             JobExecutor(),
             UIThread()
         )
+        listPdvRecyclerView = findViewById(R.id.list_pdv_recycler)
         presenter = Presenter(this, pdvProvider)
     }
 
@@ -30,7 +36,13 @@ class ListPdv : AppCompatActivity(), PdvContract.PdvViewContract {
         presenter.getAllPdv()
     }
 
-    override fun loadRecyclerView() {
+    override fun loadRecyclerView(pdv: List<PdvData>) {
+        listAdapter = ListPdvAdapter(pdv)
+        listPdvRecyclerView.apply {
+            adapter = listAdapter
+            layoutManager = LinearLayoutManager(this@ListPdv)
+            presenter.clickItemAdapter(listAdapter as RecyclerView.Adapter<RecyclerView.ViewHolder>)
+        }
     }
 
     override fun showError() {
