a27545846ba6384a3ea128479ade4e2b5668abe7
jose antonio mendoza lopez <antoniunix@gmail.com>
2019-10-09 00:52:57 -0500

    implement navigation bottom view

--- navigation/src/main/java/com/chuys/gshp/navigation/ActivityManager.kt
+++ navigation/src/main/java/com/chuys/gshp/navigation/ActivityManager.kt
@@ -2,6 +2,7 @@ package com.chuys.gshp.navigation
 
 import android.app.Activity
 import android.content.Intent
+import android.view.MenuItem
 import com.chuys.gshp.shared.domain.constant.StringConstant
 
 class ActivityManager {
@@ -13,6 +14,12 @@ class ActivityManager {
                     Intent(
                         activity.applicationContext,
                         Class.forName(StringConstant.PACKAGE_NAME + Activities.SPLASH.activity)
+                    )
+                )
+                Activities.HOME -> activity.startActivity(
+                    Intent(
+                        activity.applicationContext,
+                        Class.forName(StringConstant.PACKAGE_NAME + Activities.HOME.activity)
                     )
                 )
                 Activities.PDV_LIST -> activity.startActivity(
@@ -33,6 +40,13 @@ class ActivityManager {
                         Class.forName(StringConstant.PACKAGE_NAME + Activities.PRICE_AND_AVAILABILITY.activity)
                     )
                 )
+            }
+        }
+
+        fun changeToActivityFromMenuItem(menuItem: MenuItem, activity: Activity) {
+            when (menuItem.itemId) {
+                R.id.action_home -> changeToActivity(Activities.HOME, activity)
+                R.id.action_pdv -> changeToActivity(Activities.PDV_LIST, activity)
             }
         }
     }
