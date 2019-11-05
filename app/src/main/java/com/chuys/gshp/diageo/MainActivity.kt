06deefe331ec6f034ad2f758d0abf3562f4f4573
Leonardo Pereyra <leonardopereyra@MacBook-Air-de-Administrador.local>
2019-11-05 17:14:57 -0600

    Cambios en workmanager

--- app/src/main/java/com/chuys/gshp/diageo/MainActivity.kt
+++ app/src/main/java/com/chuys/gshp/diageo/MainActivity.kt
@@ -2,12 +2,6 @@ package com.chuys.gshp.diageo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.lifecycle.Observer
-import androidx.work.OneTimeWorkRequest
-import androidx.work.WorkInfo
-import androidx.work.WorkManager
-import com.chuys.gshp.diageo.splash.WorkerExample
 
 class MainActivity : AppCompatActivity() {
 
--- shared/src/main/java/com/chuys/gshp/shared/util/workmanager/GeolocationTrackWorkManager.kt
+++ shared/src/main/java/com/chuys/gshp/shared/util/workmanager/GeolocationTrackWorkManager.kt
@@ -2,27 +2,29 @@ package com.chuys.gshp.shared.util.workmanager
 
 import android.app.Application
 import android.util.Log
-import androidx.annotation.NonNull
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.chuys.gshp.shared.data.repository.GeolocationRepository
 
-class GeolocationTrackWorkManager( context: Application?, workerParams: WorkerParameters?) :
-    Worker(context!!, workerParams!!) {
+class GeolocationTrackWorkManager(context: Application, workerParams: WorkerParameters) :
+    Worker(context, workerParams) {
 
-    private var repository: GeolocationRepository= GeolocationRepository(context!!)
+    var repository:GeolocationRepository=GeolocationRepository(context)
 
     override fun doWork(): Result {
        Log.e("leo","do work init")
        return try {
            Log.e("leo","do work init1")
-
            repository.getLocation()
            Result.success()
        }catch (e : Exception){
            System.out.println("Init do work fall")
            Result.failure()
        }
+    }
+
+    override fun onStopped() {
+        super.onStopped()
     }
 
 
