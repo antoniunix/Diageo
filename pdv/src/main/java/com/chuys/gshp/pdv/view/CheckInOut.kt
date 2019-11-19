c114f0de798bbdea63557575c76038d67b21c1e6
Leonardo Pereyra <leonardopereyra@MacBook-Air-de-Administrador.local>
2019-11-18 21:13:34 -0600

    Add okta, json with configuration, manifest with callback

---
+++ pdv/src/main/java/com/chuys/gshp/pdv/view/CheckInOut.kt
@@ -0,0 +1,19 @@
+package com.chuys.gshp.pdv.view
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.chuys.gshp.pdv.R
+
+class CheckInOut :AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_check)
+
+
+
+    }
+
+}
