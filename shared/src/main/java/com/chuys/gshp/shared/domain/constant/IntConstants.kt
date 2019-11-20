8461c4a8bbcf9ad28c38224597ba4201103e159b
Leonardo Pereyra <leonardopereyra@MacBook-Air-de-Administrador.local>
2019-11-20 08:41:01 -0600

    Add permissos in AddPdv, force accepts

--- shared/src/main/java/com/chuys/gshp/shared/domain/constant/IntConstants.kt
+++ shared/src/main/java/com/chuys/gshp/shared/domain/constant/IntConstants.kt
@@ -2,4 +2,5 @@ package com.chuys.gshp.shared.domain.constant
 
 object IntConstants {
     const val Avaylability = 1
+    const val LOCATION_ACTIVITY_REQUEST_CODE=1000
 }
--- shared/src/main/java/com/chuys/gshp/shared/util/workmanager/GeolocationTrackWorkManager.kt
+++ shared/src/main/java/com/chuys/gshp/shared/util/workmanager/GeolocationTrackWorkManager.kt
@@ -1,14 +1,17 @@
 package com.chuys.gshp.shared.util.workmanager
 
+import android.content.ContentProvider
 import android.content.Context
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.chuys.gshp.shared.data.provider.ContextDataProvider
 import com.chuys.gshp.shared.data.repository.GeolocationDataRepository
 
 class GeolocationTrackWorkManager(context: Context, workerParams: WorkerParameters) :
     Worker(context, workerParams) {
 
-   var dataRepository:GeolocationDataRepository=GeolocationDataRepository(context)
+    var contentProvider=ContextDataProvider(context)
+   var dataRepository:GeolocationDataRepository=GeolocationDataRepository(contentProvider)
 
     override fun doWork(): Result {
        return try {
