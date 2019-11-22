2264498d70b19e2c170d25dc7a6f544b5d1de41d
Leonardo Pereyra <leonardopereyra@MacBook-Air-de-Administrador.local>
2019-11-22 10:57:28 -0600

    Add address location

--- geolocation/src/main/java/com/chuys/gshp/geolocation/view/GeolocationActivity.kt
+++ geolocation/src/main/java/com/chuys/gshp/geolocation/view/GeolocationActivity.kt
@@ -3,6 +3,7 @@ package com.chuys.gshp.geolocation.view
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
+import android.location.Address
 import android.location.Location
 import android.os.Bundle
 import android.widget.Toast
@@ -15,6 +16,7 @@ import kotlinx.android.synthetic.main.geolocation_activity.*
 
 
 class GeolocationActivity : AppCompatActivity(), GeolocationContract.GeolocationViewContract {
+
     private val LOCATION_PERMISSION_REQUEST_CODE = 1000
     private val LOCATION_ACTIVITY_REQUEST_CODE = 1000
     private lateinit var presenter: GeolocationContract.GeolocationPresenterContract
@@ -65,5 +67,10 @@ class GeolocationActivity : AppCompatActivity(), GeolocationContract.Geolocation
     override fun showLocation(location: Location) {
        Toast.makeText(this,"Location "+location.longitude+" "+location.latitude,Toast.LENGTH_LONG).show()
     }
+
+    override fun setAddres(address: Address) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 
 }
--- shared/src/main/java/com/chuys/gshp/shared/data/provider/GeolocationDataProvider.kt
+++ shared/src/main/java/com/chuys/gshp/shared/data/provider/GeolocationDataProvider.kt
@@ -1,10 +1,12 @@
 package com.chuys.gshp.shared.data.provider
 
+import android.location.Address
 import android.location.Location
 import com.chuys.gshp.shared.data.repository.GeolocationDataRepository
 import com.chuys.gshp.shared.domain.executor.PostExecutionThread
 import com.chuys.gshp.shared.domain.executor.ThreadExecutor
 import com.chuys.gshp.shared.domain.provider.GeolocationProvider
+import com.chuys.gshp.shared.domain.usecase.AddressUseCase
 import com.chuys.gshp.shared.domain.usecase.LocationUseCase
 import com.chuys.gshp.shared.domain.usecase.SingleUseCase
 
@@ -12,12 +14,15 @@ class GeolocationDataProvider(
     private val jobExecutor: ThreadExecutor,
     private val uiThread: PostExecutionThread,
     private val contextDataProvider: ContextDataProvider
-) :GeolocationProvider{
+) : GeolocationProvider {
+    private val geolocationDataRepository = GeolocationDataRepository(contextDataProvider)
 
-    private val geolocationDataRepository =GeolocationDataRepository(contextDataProvider.getContext())
+    override fun getUserLocation(): SingleUseCase<Any, Location> {
+        return LocationUseCase(geolocationDataRepository, jobExecutor, uiThread)
+    }
 
-    override fun getUserLocation():SingleUseCase<Any, Location> {
-       return LocationUseCase(geolocationDataRepository,jobExecutor,uiThread)
+    override fun getAddres(lat: Double, lon: Double): SingleUseCase<Any, Address> {
+        return AddressUseCase(geolocationDataRepository, jobExecutor, uiThread,lat,lon)
     }
 
 
