645a7348d9ebd867dae83f5455eecd4bb9b53a9e
LeonardoPC23 <pereyraleonardo23@gmail.com>
2019-12-09 03:33:25 -0600

    Media provider

--- comunication/src/main/java/com/chuys/gshp/comunication/view/data/provider/MediaDataProvider.kt
+++ comunication/src/main/java/com/chuys/gshp/comunication/view/data/provider/MediaDataProvider.kt
@@ -1,12 +1,18 @@
 package com.chuys.gshp.comunication.view.data.provider
 
+import com.chuys.gshp.comunication.view.domain.model.MediaModel
 import com.chuys.gshp.comunication.view.domain.provider.MediaProvider
 import com.chuys.gshp.shared.domain.executor.PostExecutionThread
 import com.chuys.gshp.shared.domain.executor.ThreadExecutor
+import com.chuys.gshp.shared.domain.models.Resource
+import com.chuys.gshp.shared.domain.usecase.SingleUseCase
 
 class MediaDataProvider (
     private val jobExecutor: ThreadExecutor,
     private val uiThread: PostExecutionThread
 ):MediaProvider{
+    override fun getLisMediaUseCase(): SingleUseCase<Any, Resource<List<MediaModel>>> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 }
