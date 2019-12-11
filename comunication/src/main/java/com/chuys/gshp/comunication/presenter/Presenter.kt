8adbb98b601e9a5b2137542aae15d59bc95f71e7
LeonardoPC23 <pereyraleonardo23@gmail.com>
2019-12-11 05:39:42 -0600

    Name temporal,

--- comunication/src/main/java/com/chuys/gshp/comunication/presenter/Presenter.kt
+++ comunication/src/main/java/com/chuys/gshp/comunication/presenter/Presenter.kt
@@ -10,6 +10,7 @@ import com.chuys.gshp.comunication.presenter.contract.MediaContract
 import com.chuys.gshp.comunication.view.MediaAdapter
 import com.chuys.gshp.shared.domain.provider.ContextProvider
 import io.reactivex.disposables.CompositeDisposable
+import kotlinx.android.synthetic.main.row_media.view.*
 
 class Presenter(val view: MediaContract.MediaViewContract, val mediaProvider: MediaProvider,val contextProvider: ContextProvider) :
     MediaContract.MediaPresenterContract {
@@ -37,11 +38,22 @@ class Presenter(val view: MediaContract.MediaViewContract, val mediaProvider: Me
     ) {
         val listAdapter = adapter as MediaAdapter
         disposables.add(listAdapter.clickEvent.subscribe { it ->
-            val file = createFile("FILE.pdf",contextProvider.getContext())
+            val file = createFile(createNameTemp(it),contextProvider.getContext())
             view.initDownLoadManager(initDownLoadRequest(it.url, it.title, file))
 
         })
     }
 
-
+    private fun createNameTemp(media:MediaModel):String{
+        val part=media.title.split("\\s".toRegex())[0]
+        return when (media.mediaType) {
+            1 ->  part+".pdf"
+            2 ->  part+".jpg"
+            3 ->  part+".png"
+            4 ->  part+".mp4"
+            5 ->  part+".html"
+            6 ->  part+".docx"
+            else -> "Otros"
+        }
+    }
 }
--- comunication/src/main/java/com/chuys/gshp/comunication/view/MediaAdapter.kt
+++ comunication/src/main/java/com/chuys/gshp/comunication/view/MediaAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.chuys.gshp.comunication.R
 import com.chuys.gshp.comunication.domain.model.MediaModel
+import com.chuys.gshp.shared.domain.constant.IntConstants
 import io.reactivex.Observable
 import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.row_media.view.*
@@ -15,15 +16,15 @@ class MediaAdapter(val items: List<MediaModel>) :
 
     private val clickSubject = PublishSubject.create<MediaModel>()
     val clickEvent: Observable<MediaModel> = clickSubject
+    var mediaType: Int = 0
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderListMedia {
-        items.groupBy { it.mediaType }
         return ViewHolderListMedia(
             LayoutInflater.from(parent.context).inflate(
-                R.layout.row_media, parent, false)
+                R.layout.row_media, parent, false
+            )
         )
-
     }
 
     override fun getItemCount(): Int {
@@ -36,15 +37,27 @@ class MediaAdapter(val items: List<MediaModel>) :
 
     inner class ViewHolderListMedia(view: View) : RecyclerView.ViewHolder(view) {
         init {
-
             itemView.setOnClickListener {
                 clickSubject.onNext(items[layoutPosition])
             }
         }
 
         fun bin(media: MediaModel) {
-            itemView.txt_description.text = media.description
-            itemView.txt_title.text = media.title
+            if (mediaType != media.mediaType) {
+               when (media.mediaType) {
+                    1 ->  itemView.txt_title.text="PDF"
+                    2 ->  itemView.txt_title.text="JPG"
+                    3 ->  itemView.txt_title.text="PNG"
+                    4 ->  itemView.txt_title.text="MP4"
+                    5 ->  itemView.txt_title.text="HTML"
+                    6 ->  itemView.txt_title.text="DOCX"
+                    else -> itemView.txt_title.text= "Otros"
+                }
+            } else {
+                itemView.txt_description.text = media.description
+                itemView.txt_title.text = media.title
+            }
+            mediaType=media.mediaType
         }
     }
 }
