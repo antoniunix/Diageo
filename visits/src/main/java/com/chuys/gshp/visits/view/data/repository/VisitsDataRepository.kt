46db35196c4286b73d7283db44e52e9ef8282318
jose antonio mendoza lopez <antoniunix@gmail.com>
2019-12-11 01:47:18 -0600

    connect repository visits

--- visits/src/main/java/com/chuys/gshp/visits/view/data/repository/VisitsDataRepository.kt
+++ visits/src/main/java/com/chuys/gshp/visits/view/data/repository/VisitsDataRepository.kt
@@ -1,5 +1,6 @@
 package com.chuys.gshp.visits.view.data.repository
 
+import com.chuys.gshp.shared.data.database.realtime.RealmTimeDbConfig
 import com.chuys.gshp.shared.data.database.sqlite.provider.SqliteDBProvider
 import com.chuys.gshp.shared.domain.constant.StringConstant
 import com.chuys.gshp.shared.domain.models.Resource
@@ -9,19 +10,10 @@ import io.reactivex.Single
 
 class VisitsDataRepository : VisitsRepository {
 
-    private val sqliteHelper = SqliteDBProvider.SqliteDBHelper.getDbHelper()
-    private val tableName = "item"
 
     override fun getPdvVisited(): Single<Resource<List<VisitModel>>> {
-        val visist = VisitModel(
-            1, 1, 1234567892, 9876543211,
-            1, "sdfghjk", 0, "pdv"
-        )
-        val visist1 = VisitModel(
-            1, 1, 1234567893, 9876543212,
-            1, "sdfghjk", 0, "pdv1"
-        )
-        return Single.just(listOf(visist, visist1)).map { result ->
+
+        return Single.just(VisitsEntity().getItem()).map { result ->
             when {
                 !result.isEmpty() -> {
                     return@map Resource.success(result, StringConstant.EMPTY_STRING)
